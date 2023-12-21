21f9fc69a4879a76e59f26bdd0b4e668fcf12293
Marcelo Macharelli <marcelomacharelli@gmail.com>
2023-12-21 14:58:18 -0300

    Service layer, component registration

--- src/main/java/com/devsuperior/course/repositories/UserRepository.java
+++ src/main/java/com/devsuperior/course/repositories/UserRepository.java
@@ -1,6 +1,5 @@
 package com.devsuperior.course.repositories;
 
-import org.springframework.boot.autoconfigure.data.jpa.JpaRepositoriesAutoConfiguration;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.devsuperior.course.entities.User;
